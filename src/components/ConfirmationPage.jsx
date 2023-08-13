974df8b0bb12573abf81af349bf03aef47b30e8e
hadep275 <hadep275@gmail.com>
2023-08-13 01:37:14 +0100

    Confirmation Page Error handling

--- src/components/ConfirmationPage.jsx
+++ src/components/ConfirmationPage.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styles from "./styles/ConfirmationPage.module.css";
+import OrderConfirmation from "./OrderConfirmation";
 
 function ConfirmationPage() {
+  const { id } = useParams();
+  const [order, setOrder] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchOrder = async (orderId) => {
+    try {
+      const response = await fetch(
+        `http://localhost:3001/api/orders/${orderId}`
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setOrder(data);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchOrder(id);
+  }, [id]);
+
   return (
-    <div>
-      <h2>Order Confirmation</h2>
-      {/* <p>Order ID: {orderData.id}</p> */}
+    <div className={styles.confirmationPage}>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {order && <OrderConfirmation order={order} />}
     </div>
-  )
+  );
 }
 
-export default ConfirmationPage
-
+export default ConfirmationPage;
